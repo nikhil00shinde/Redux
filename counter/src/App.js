3c8bf91e56d049388cb63a3279dd01dcf03524b1
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-12-19 14:27:52 +0530

    increment button working

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { incrementCreator } from "./redux/actions";
 
 let App = () => {
+	//useSelector -> ek function leta hain jo hamare store se reducer se current state fetch karta hain
+	let state = useSelector((state) => state);
+
+	//useDispatch -> ek hook hain jo ek function return karata hain
+	let dispatch = useDispatch();
 	return (
 		<div>
-			<button>+</button>
-			<p>0</p>
+			<button
+				onClick={() => {
+					//ye ek object leta hain and store ko dispatch karta hain
+					//store mei hamare pass reducer ke action object mei ata hain ye incrementCreator ka object
+
+					//aur jo bhi reducer se return hota woh value state mei update hoti hain, aur update hone par store provider ko btata hain ki state update hooyi jis se hamara pura app re-render hota hain
+					dispatch(incrementCreator());
+				}}
+			>
+				+
+			</button>
+			<p>{state}</p>
 		</div>
 	);
 };
--- counter/src/redux/reducer.js
+++ counter/src/redux/reducer.js
@@ -3,6 +3,9 @@
 //2 argument leta hai
 //first one => intial state hoti hain (default value)
 //second one => action ka object ata hain jiske bases pe state ko change krega
+
+//actions dispatch karege toh state change kar
+
 let reducer = (state = 0, action) => {
 	switch (action.type) {
 		case "INCREMENT":
